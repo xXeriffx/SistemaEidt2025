ed751e1eaa6618e4c1a9332705f0b064f264ed63
xXeriffx <giulianomoraes@gmail.com>
2025-09-22 21:23:50 -0400

    Quinto commit: Atualizei o Util, porque estava faltando bastante coisa, por favor leia os comentarios.

--- src/tools/Util_GME.java
+++ src/tools/Util_GME.java
@@ -4,8 +4,13 @@
  */
 package tools;
 
+import java.util.Date;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JFormattedTextField;
 import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 /**
@@ -29,14 +34,31 @@ public class Util_GME {
     //}
     //Com instanceof
     public static void Limpar(JComponent ...componentes){
-       for(int i = 0; i < componentes.length; i ++){
-           //instanceof
-          if (componentes[i] instanceof JTextField) {
-              ((JTextField) componentes[i]).setText("");
-          }
-          
+        for(int i = 0; i < componentes.length; i ++){
+           
+        //instanceof
+        if (componentes[i] instanceof JTextField) {
+            ((JTextField) componentes[i]).setText("");
         }
+          
+        if (componentes[i] instanceof JComboBox){
+            ((JComboBox) componentes[i]).setSelectedIndex(-1);
+        }
+        
+        if (componentes[i] instanceof JFormattedTextField){
+            ((JFormattedTextField) componentes[i]).setText("");
+        }
+        
+        if (componentes[i] instanceof JPasswordField){
+            ((JPasswordField) componentes[i]).setText("");
+        }
+        
+        if (componentes[i] instanceof JCheckBox){
+            ((JCheckBox) componentes[i]).setSelected(false);
+        }
+      }
     }
+    
     public static void messagem(String cad){
         JOptionPane.showMessageDialog(null,cad);
     }
@@ -44,7 +66,7 @@ public class Util_GME {
     public static boolean perguntar(String cad){
         JOptionPane.showConfirmDialog(null, cad);
         return true;
-        //se o botao for "yes" returna true se nao retorna false 
+        //se o botao for "yes" returna true /// se nao retorna false (palavras ditas em sala)
     }
     
     public static int strToInt(String num){
@@ -53,5 +75,25 @@ public class Util_GME {
     
     public static String intToStr(String cad){
         return String.valueOf(cad);
+    }
+    
+    public static double strToDouble(String cad){
+    return Double.valueOf(cad);
+    }
+    
+    
+    //O lucas Boeira me ajudou nessa parte, eu usei o chat mas não parecia em nada com oque a gente viu na sala. 
+    
+    public static Date strToDate(String cad){
+        
+    try {
+        return new java.text.SimpleDateFormat("dd/MM/yyyy").parse(cad);//obrigatorio estar formatado os campos [date] se não da B.O. (eu acho, ainda nao testei no banco)
+    } catch (Exception e) { //cath para se a STR estiver sem formatção não crashar o progama.
+        return null;
+    }
+    
+    }
+    public static String dateToStr(Date data){
+        return new java.text.SimpleDateFormat("dd/MM/yyyy").format(data);
     }
 }
